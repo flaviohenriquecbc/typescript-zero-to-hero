4cd4a57b0de4f2c9f56c9cf00e28e42f82bb8f71
Flavio Freitas <flaviohenriquecbc@gmail.com>
2020-05-14 20:21:14 +0200

    Add webpackcc

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import { ProjectInput } from './drag-drop-project/components/project-input'
 import { ProjectList } from './drag-drop-project/components/project-list'
 
+declare const GLOBAL: string
+
+console.log(GLOBAL)
+
 new ProjectInput()
 new ProjectList('active')
 new ProjectList('finished')
