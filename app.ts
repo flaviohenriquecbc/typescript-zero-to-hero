5b3da90c1b63324acf41efd55721dbb2f6d63017
Flavio Freitas <flaviohenriquecbc@gmail.com>
2020-05-14 20:51:01 +0200

    Add class-transformer and class-validator

--- app.ts
+++ app.ts
@@ -1,10 +1,26 @@
-import { ProjectInput } from './drag-drop-project/components/project-input'
-import { ProjectList } from './drag-drop-project/components/project-list'
+import 'reflect-metadata'
+import { plainToClass } from 'class-transformer'
+import { validate } from 'class-validator'
 
-declare const GLOBAL: string
+import { Product } from './class-utils/Product.model'
 
-console.log(GLOBAL)
+const products = [
+    { title: 'A Carpet', price: 29.99 },
+    { title: 'A Book', price: 10.99 }
+]
 
-new ProjectInput()
-new ProjectList('active')
-new ProjectList('finished')
+const loadedProducts = plainToClass(Product, products)
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation())
+}
+
+const newProd = new Product('', -5.99)
+validate(newProd).then(errors => {
+    if(errors.length > 0) {
+        console.log('VALIDATION ERROS!')
+        console.log(errors)
+    } else {
+        console.log(newProd.getInformation())
+    }
+})
